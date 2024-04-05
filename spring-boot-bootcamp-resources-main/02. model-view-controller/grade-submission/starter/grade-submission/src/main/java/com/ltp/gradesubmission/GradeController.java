a59c9feed2a90d757e94c822070c9b909230e939
Jiayi Huang <huangjiayi@JiayideAir.lan>
2024-04-05 14:13:26 -0700

    2024.4.5

--- spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -8,16 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry","Potions","C-"),
-        new Grade("Alex","CS","A"),
-        new Grade("Ben","EE","B+")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
     
     @GetMapping("/grades")
     public String getGrades(Model model){
@@ -25,8 +24,30 @@ public class GradeController {
         return "grades";
     }
 
-    @GetMapping("/shows")
-    public String getShows(){
-        return "shows";
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required = false) String id){
+        int index = getGradeIndex(id);
+        model.addAttribute("grade",index==Constants.NOT_FOUND ? new Grade():studentGrades.get(index));
+        return "form";
     }
+
+    @PostMapping("handleSubmit")
+    public String submitForm(Grade grade) {
+        int index = getGradeIndex(grade.getId());
+        if(index==Constants.NOT_FOUND){
+            studentGrades.add(grade);
+        }else{
+            studentGrades.set(index, grade);
+        }
+        return "redirect:/grades";
+    }
+    public Integer getGradeIndex(String id){
+        for(int i=0;i<studentGrades.size();i++){
+            if(studentGrades.get(i).getId().equals(id)){
+                return i;
+            }
+        }
+        return Constants.NOT_FOUND;
+    }
+
 }
