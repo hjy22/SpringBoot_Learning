aec615c7af6f93ba28213d2f030ed302fb6c5dac
Jiayi Huang <huangjiayi@JiayideAir.lan>
2024-04-06 21:12:18 -0700

    2024.4.6

--- spring-boot-bootcamp-resources-main/02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
@@ -2,4 +2,11 @@ package com.ltp.globalsuperstore;
 
 public class Constants {
     public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
+
+    public static final int NOT_FOUND = 404;
+
+    public static final String SUCCESS_STATUS = "success";
+
+    public static final String FAILED_STATUS = "failed";
+
 }
