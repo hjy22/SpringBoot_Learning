1dafaaa4d6533c92d11adba99a99131b5de27666
Jiayi Huang <huangjiayi@JiayideAir.lan>
2024-04-10 23:17:05 -0700

    2024.4.10

--- spring-boot-bootcamp-resources-main/06. testing/grade-submission/starter/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ spring-boot-bootcamp-resources-main/06. testing/grade-submission/starter/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -1,6 +1,8 @@
 package com.ltp.gradesubmission;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -12,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import com.ltp.gradesubmission.Constants;
 import com.ltp.gradesubmission.pojo.Grade;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.service.GradeService;
@@ -37,7 +38,6 @@ public class GradeServiceTest {
 
         assertEquals("Harry", result.get(0).getName());
         assertEquals("Arithmancy", result.get(1).getSubject());
-
     }
 
     @Test
@@ -51,5 +51,37 @@ public class GradeServiceTest {
 
         assertEquals(0, valid);
         assertEquals(Constants.NOT_FOUND, notFound);
+    }
+
+    @Test
+    public void getGradeByIdTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        Grade valid = gradeService.getGradeById(grade.getId());
+        assertEquals(valid, grade);
+    }
+
+    @Test
+    public void addGradeTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        Grade newGrade = new Grade("Hermione","Arithmancy","A+");
+        gradeService.submitGrade(newGrade);
+        verify(gradeRepository,times(1)).addGrade(newGrade);
+    }
+
+    @Test
+    public void updateGradeTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        grade.setScore("A-");
+        gradeService.submitGrade(grade);
+        verify(gradeRepository,times(1)).updateGrade(grade, 0);
     }
 }
