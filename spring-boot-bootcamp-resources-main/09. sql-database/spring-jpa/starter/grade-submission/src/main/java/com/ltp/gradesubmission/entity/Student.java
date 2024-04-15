a0a8851e9d46632a8228398eebea771d0475dbbb
Jiayi Huang <huangjiayi@JiayideAir.lan>
2024-04-14 23:15:28 -0700

    2024.4.14

--- spring-boot-bootcamp-resources-main/09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -2,10 +2,29 @@ package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// 声明这是一个 JPA 实体类
+@Entity
+@Table(name = "student")
 public class Student {
 
+    //声明表的primary key
+    @Id
+    // 声明该字段的值由数据库自动生成，添加元素后递增
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+    
+    @Column(name = "birth_date" , nullable = false)
     private LocalDate birthDate;
 
     public Long getId() {
